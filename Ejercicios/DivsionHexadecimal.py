514221fbb5b9ec5d4284e8234e9d44b48a1e136b
Santiago Maldonado <santiagomaldonadorojas64@gmail.com>
2023-01-24 15:42:43 -0500

    Metodo de la resta funciona, división da datos erroneos

--- Ejercicios/DivsionHexadecimal.py
+++ Ejercicios/DivsionHexadecimal.py
@@ -1,4 +1,5 @@
 #Funciones
+#Aun no funciona
 #Conversor de decimal a hexadecimal
 def decimal_to_hex(decimal):
     hex_digits = "0123456789ABCDEF"
@@ -8,23 +9,23 @@ def decimal_to_hex(decimal):
         decimal = decimal // 16
         result = hex_digits[remainder] + result
     return result
-#Resta de Hexadecimales
-def hex_rest(a, b):
+#Division de Hexadecimales
+def hex_div(a, b):
     decimal_a = int(a, 16)
     decimal_b = int(b, 16)
     decimal_sum = decimal_a - decimal_b
-    hex_rest = decimal_to_hex(decimal_sum)
-    return hex_rest
+    hex_div = decimal_to_hex(decimal_sum)
+    return hex_div
 while True:
     #Ingreso de variables
     num1 = input("Ingresa el primer número hexadecimal: ")
     num2 = input("Ingresa el segundo número hexadecimal: ")
     #resultado 
-    result = hex_rest(num1, num2)
+    result = hex_div(num1, num2)
     print (result)
-    option = int(input("¿Quieres restar hexadecimales?  \n 1) Si \n 2) no \n Ingresa el número de la opción: "))
+    option = int(input("¿Quieres dividir hexadecimales?  \n 1) Si \n 2) no \n Ingresa el número de la opción: "))
     if option ==1:
-        hex_rest(num1, num2)
+        hex_div(num1, num2)
     if option ==2:
         break
     else:
