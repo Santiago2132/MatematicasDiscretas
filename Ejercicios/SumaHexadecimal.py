14188889e616ad5909132a5e50abe2f1d9852258
Santiago Maldonado <santiagomaldonadorojas64@gmail.com>
2023-01-24 22:03:46 -0500

    Suma de Hexadecimal terminada sin interfaz garfica

--- Ejercicios/SumaHexadecimal.py
+++ Ejercicios/SumaHexadecimal.py
@@ -8,6 +8,16 @@ def decimal_to_hex(decimal):
         decimal = decimal // 16
         result = hex_digits[remainder] + result
     return result
+#Verificacion de números hexadecimales
+def is_hex_number(string):
+    if not string:
+        return False
+    hex_digits = set("0123456789abcdefABCDEF")
+    for char in string:
+        if char not in hex_digits:
+            return False
+    return True
+
 #Suma de Hexadecimales
 def hex_sum(a, b):
     decimal_a = int(a, 16)
@@ -16,9 +26,22 @@ def hex_sum(a, b):
     hex_sum = decimal_to_hex(decimal_sum)
     return hex_sum
 while True:
-    #Ingreso de variables
-    num1 = input("Ingresa el primer número hexadecimal: ")
-    num2 = input("Ingresa el segundo número hexadecimal: ")
+    #Impresión del bienvenido
+    print("\nBienvenido a la suma Hexadecimal \n∵*.•´¸.•*´✶´ \n\n° ☆ ° ˛*˛☆_Π______˚☆\n*˚ ˛★˛•*/________/ ~ ⧹。˚ ˚\n˚ ˛•˛•˚  ｜ 田田 ｜門｜ ˚\n╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬")
+    #Ingreso de datos
+
+    num1 = input("\nIngresa el primer número hexadecimal: ")
+    if is_hex_number(num1)==False:
+        while True:            
+            num1 = input("\nIngresa el primer número hexadecimal: ")
+            if is_hex_number(num1)==True:
+                break
+    num2 = input("\nIngresa el segundo número hexadecimal: ")
+    if is_hex_number(num2)==False:
+        while True:            
+            num2 = input("\nIngresa el segundo número hexadecimal: ")
+            if is_hex_number(num2)==True:
+                break                       
     #resultado 
     result = hex_sum(num1, num2)
     print (result)
