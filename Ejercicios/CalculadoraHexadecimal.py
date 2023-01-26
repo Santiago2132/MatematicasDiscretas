6e72bfcdff1ad2cc2de9462b3638980a7ab5a256
Santiago Maldonado <santiagomaldonadorojas64@gmail.com>
2023-01-26 11:46:05 -0500

    Se cambio el codigo de la calculadora

--- Ejercicios/CalculadoraHexadecimal.py
+++ Ejercicios/CalculadoraHexadecimal.py
@@ -1,42 +1,42 @@
 #Funciones
 #Conversor de decimal a hexadecimal
 def decimal_to_hex(decimal):
-    hex_digits = "0123456789ABCDEF"
-    result = ""
-    while decimal > 0:
-        remainder = decimal % 16
-        decimal = decimal // 16
-        result = hex_digits[remainder] + result
-    return result
+   hex_digits = "0123456789ABCDEF"
+   result = ""
+   while decimal > 0:
+      remainder = decimal % 16
+      decimal = decimal // 16
+      result = hex_digits[remainder] + result
+   return result
 #Suma de Hexadecimales
 def hex_sum(a, b):
-    decimal_a = int(a, 16)
-    decimal_b = int(b, 16)
-    decimal_sum = decimal_a + decimal_b
-    hex_sum = decimal_to_hex(decimal_sum)
-    return hex_sum
+   decimal_a = int(a, 16)
+   decimal_b = int(b, 16)
+   decimal_sum = decimal_a + decimal_b
+   hex_sum = decimal_to_hex(decimal_sum)
+   return hex_sum
 #Resta de hexadecimales
 
 def hex_rest(a, b):
-    decimal_a = int(a, 16)
-    decimal_b = int(b, 16)
-    decimal_sum = decimal_a - decimal_b
-    hex_rest = decimal_to_hex(decimal_sum)
-    return hex_rest
+   decimal_a = int(a, 16)
+   decimal_b = int(b, 16)
+   decimal_sum = decimal_a - decimal_b
+   hex_rest = decimal_to_hex(decimal_sum)
+   return hex_rest
 #Multiplicacion de hexadecimales
 def hex_multiply(a, b):
-    decimal_a = int(a, 16)
-    decimal_b = int(b, 16)
-    decimal_multiply = decimal_a * decimal_b
-    hex_rest = decimal_to_hex(decimal_multiply)
-    return hex_rest
+   decimal_a = int(a, 16)
+   decimal_b = int(b, 16)
+   decimal_multiply = decimal_a * decimal_b
+   hex_rest = decimal_to_hex(decimal_multiply)
+   return hex_rest
 #Division de hexadecimales
 def hex_division(a, b):
-    decimal_a = int(a, 16)
-    decimal_b = int(b, 16)
-    decimal_division = decimal_a / decimal_b
-    hex_rest = decimal_to_hex(decimal_division)
-    return hex_rest
+   decimal_a = int(a, 16)
+   decimal_b = int(b, 16)
+   decimal_division = decimal_a / decimal_b
+   hex_rest = decimal_to_hex(decimal_division)
+   return hex_rest
 #Entradas de hexadecimales
 num1 = input("Ingresa el primer número hexadecimal: ")
 num2 = input("Ingresa el segundo número hexadecimal: ")
@@ -76,17 +76,14 @@ while True:
    elif opcion == 3:    #Multiplica los dos numeros
       print(" ")
       print("RESULTADO: El producto de",num1,"*",num2,"es igual a",hex_multiply(num1,num2))
-    
-    #No funciona la division en hexadecimal
+#No funciona la division en hexadecimal
 
    #elif opcion == 4:    #Divide los dos numeros
-    #    print("RESULTADO: El producto de",num1,"/",num2,"es igual a",hex_division(num1,num2))
+   #    print("RESULTADO: El producto de",num1,"/",num2,"es igual a",hex_division(num1,num2))
 
-   elif opcion == 5:       #Vuelve a preguntar los dos numeros
-      
-        num1 = input("Introduce tu primer número hexadecimal: ")
-    
-        num2 = input("Introduce tu segundo número: ")
+   elif opcion == 5:       #Vuelve a preguntar los dos numeros      
+      num1 = input("Introduce tu primer número hexadecimal: ")    
+      num2 = input("Introduce tu segundo número: ")
                      
 
    elif opcion == 6:    #Cierra la calculadora
