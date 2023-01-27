14c644b256ad6838c3addc274ca7486f08e9169a
Santiago Maldonado <santiagomaldonadorojas64@gmail.com>
2023-01-27 01:11:04 -0500

    Se añadio la calculadora de matrices

---
+++ Ejercicios/CalculadoraMatrices.py
@@ -0,0 +1,50 @@
+#Realizar una calculadora de matrices en la que
+#Se multipliquen, sumen, resten y hagan
+# el producto punto de las matrices
+
+print("\n∵*.•´¸.•*´✶´♡\n° ☆ ° ˛*˛☆_Π______˚☆\n*˚ ˛★˛•*/________/ ~ ⧹。˚ ˚\n˚ ˛•˛•˚  ｜ 田田 ｜門｜ ˚\n╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬\n\nBienvenido a la calculadora de matrices")
+
+#Primera Matriz
+while True:
+    try:
+        m1 = int(input("\nIngresa el valor de columnas de la primera Matriz: "))
+        break
+    except ValueError:
+        print("Ingrese números enteros")
+
+while True:
+    try:
+        n1 = int(input("\nIngresa el valor de filas de la primera Matriz: "))
+        break
+    except ValueError:
+        print("Ingrese números enteros")
+#Segunda Matriz
+
+while True:
+    try:
+        m2 = int(input("\nIngresa el valor de columnas de la segunda Matriz: "))
+        break
+    except ValueError:
+        print("Ingrese números enteros")
+
+while True:
+    try:
+        n2 = int(input("\nIngresa el valor de filas de la segunda Matriz: "))
+        break
+    except ValueError:
+        print("Ingrese números enteros")
+
+
+# Suma de matrices
+
+
+
+# Resta de matrices
+
+
+
+# Multiplicación de matrices
+
+
+
+# Multiplicacion de un escalar por una matriz
