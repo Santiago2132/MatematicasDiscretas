aca560695593a88ae27baf4f6eaefde5c249ed6a
Santiago Maldonado <santiagomaldonadorojas64@gmail.com>
2023-01-27 01:56:07 -0500

    La calculadora ya funciona en su totalidad

--- Ejercicios/CalculadoraHexadecimal.py
+++ Ejercicios/CalculadoraHexadecimal.py
@@ -1,5 +1,18 @@
 #Funciones
+
+#Verificacion de números hexadecimales
+
+def ishexnumber(string):
+   if not string:
+      return False
+   hex_digits = set("0123456789abcdefABCDEF")
+   for char in string:
+      if char not in hex_digits:
+            return False
+   return True
+
 #Conversor de decimal a hexadecimal
+
 def decimal_to_hex(decimal):
    hex_digits = "0123456789ABCDEF"
    result = ""
@@ -8,13 +21,16 @@ def decimal_to_hex(decimal):
       decimal = decimal // 16
       result = hex_digits[remainder] + result
    return result
+
 #Suma de Hexadecimales
+
 def hex_sum(a, b):
    decimal_a = int(a, 16)
    decimal_b = int(b, 16)
    decimal_sum = decimal_a + decimal_b
    hex_sum = decimal_to_hex(decimal_sum)
    return hex_sum
+
 #Resta de hexadecimales
 
 def hex_rest(a, b):
@@ -23,34 +39,52 @@ def hex_rest(a, b):
    decimal_sum = decimal_a - decimal_b
    hex_rest = decimal_to_hex(decimal_sum)
    return hex_rest
+
 #Multiplicacion de hexadecimales
+
 def hex_multiply(a, b):
+
    decimal_a = int(a, 16)
    decimal_b = int(b, 16)
    decimal_multiply = decimal_a * decimal_b
    hex_rest = decimal_to_hex(decimal_multiply)
    return hex_rest
+
 #Division de hexadecimales
+
 def hex_division(a, b):
    decimal_a = int(a, 16)
    decimal_b = int(b, 16)
-   decimal_division = decimal_a / decimal_b
+   decimal_division = decimal_a // decimal_b
    hex_rest = decimal_to_hex(decimal_division)
    return hex_rest
-#Entradas de hexadecimales
-num1 = input("Ingresa el primer número hexadecimal: ")
-num2 = input("Ingresa el segundo número hexadecimal: ")
+
+
 
 #Opciones de la calculadora
 opcion = 0
-print("───────────────────────────────\n───────────────████─███────────\n──────────────██▒▒▒█▒▒▒█───────\n─────────────██▒────────█──────\n─────────██████──██─██──█──────\n────────██████───██─██──█──────\n────────██▒▒▒█──────────███────\n────────██▒▒▒▒▒▒───▒──██████───\n───────██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒███─\n──────██▒▒▒▒─────▒▒▒▒▒▒▒▒▒▒▒▒█─\n──────██▒▒▒───────▒▒▒▒▒▒▒█▒█▒██\n───────██▒▒───────▒▒▒▒▒▒▒▒▒▒▒▒█\n────────██▒▒─────█▒▒▒▒▒▒▒▒▒▒▒▒█\n────────███▒▒───██▒▒▒▒▒▒▒▒▒▒▒▒█\n─────────███▒▒───█▒▒▒▒▒▒▒▒▒▒▒█─\n────────██▀█▒▒────█▒▒▒▒▒▒▒▒██──\n──────██▀██▒▒▒────█████████────\n────██▀███▒▒▒▒────█▒▒██────────\n█████████▒▒▒▒▒█───██──██───────\n█▒▒▒▒▒▒█▒▒▒▒▒█────████▒▒█──────\n█▒▒▒▒▒▒█▒▒▒▒▒▒█───███▒▒▒█──────\n█▒▒▒▒▒▒█▒▒▒▒▒█────█▒▒▒▒▒█──────\n██▒▒▒▒▒█▒▒▒▒▒▒█───█▒▒▒███──────\n─██▒▒▒▒███████───██████────────\n──██▒▒▒▒▒██─────██─────────────\n───██▒▒▒██─────██──────────────\n────█████─────███──────────────\n────█████▄───█████▄────────────\n──▄█▓▓▓▓▓█▄─█▓▓▓▓▓█▄───────────\n──█▓▓▓▓▓▓▓▓██▓▓▓▓▓▓▓█──────────\n──█▓▓▓▓▓▓▓▓██▓▓▓▓▓▓▓█──────────\n──▀████████▀▀███████▀──────────") 
+
 while True: 
+   print("\nBienvenido a la calculadora Hexadecimal \n∵*.•´¸.•*´✶´ \n\n° ☆ ° ˛*˛☆_Π______˚☆\n*˚ ˛★˛•*/________/ ~ ⧹。˚ ˚\n˚ ˛•˛•˚  ｜ 田田 ｜門｜ ˚\n╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬")
+#Entradas de hexadecimales
+   num1 = input("\nIngresa el primer número hexadecimal: ")#Primer hexadecimal
+   if ishexnumber(num1)==False:        
+      while True:
+         print("\nInvalid")          
+         num1 = input("\nIngresa el primer número hexadecimal: ")
+         if ishexnumber(num1)==True:
+               break
+   num2 = input("\nIngresa el segundo número hexadecimal: ")#Segundo hexadecimal
+   if ishexnumber(num2)==False:        
+      while True:
+         print("\nInvalid")         
+         num2 = input("\nIngresa el segundo número hexadecimal: ")
+         if ishexnumber(num2)==True:
+               break   
    
    print("""
-   Bienvenido
+   Opciones:
 
-   Dime, ¿qué quieres hacer?
-   
    1) Sumar los dos números
    2) Restar los dos números
    3) Multiplicar los dos números
@@ -59,7 +93,7 @@ while True:
    6) Apagar calculadora
 
    """) #Pregunta que es lo que quiere hacer
-   while True:  
+   while True:  #Ingreso de opción
       try: 
          opcion = int(input("Elige una opción: ") )
          break
@@ -78,8 +112,8 @@ while True:
       print("RESULTADO: El producto de",num1,"*",num2,"es igual a",hex_multiply(num1,num2))
 #No funciona la division en hexadecimal
 
-   #elif opcion == 4:    #Divide los dos numeros
-   #    print("RESULTADO: El producto de",num1,"/",num2,"es igual a",hex_division(num1,num2))
+   elif opcion == 4:    #Divide los dos numeros
+      print("RESULTADO: El producto de",num1,"/",num2,"es igual a",hex_division(num1,num2))
 
    elif opcion == 5:       #Vuelve a preguntar los dos numeros      
       num1 = input("Introduce tu primer número hexadecimal: ")    
