31fa1f677330ba0f2cafe877ed4a3ff8996069ec
Santiago Maldonado <santiagomaldonadorojas64@gmail.com>
2023-02-07 08:20:17 -0500

    cambios

---
+++ Ejercicios/Bolos.py
@@ -0,0 +1,7 @@
+lista = [None]*10
+
+lista[0] = int(input("Ingresa un número: "))
+lista[1] = 2*lista[0]
+print (lista)
+if matrix[i][j]<= 10 or matrix[i][j] >= 0:
+if matrix[i][j] <= tope and matrix[i][j] >= 0:
