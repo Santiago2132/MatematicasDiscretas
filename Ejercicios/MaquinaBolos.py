73ff49a9051c0d81ea62ea4d19f3177f34eacbe5
Santiago Maldonado <santiagomaldonadorojas64@gmail.com>
2023-02-11 10:25:34 -0500

    Se borro el codigo

--- Ejercicios/MaquinaBolos.py
+++ Ejercicios/MaquinaBolos.py
@@ -1,19 +1,1 @@
-import numpy as np
-#funciones
-def strike (tiro1,tiro2):
-    if tiro1 ==10 or tiro2 ==10:
-        return True
-    return False
-def tiro(turno):
-    if turno >0:
-        return 0
-    if turno < 0:
-        return 1
-#Datos
-
-#lanzamiento 10
-
-#Puntaje
-
-resultado = np.empty((10, 2))
 
