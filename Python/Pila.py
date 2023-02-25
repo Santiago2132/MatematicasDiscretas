63e8d1fdafb1392b2f460ae1af740e0b1bb13ebc
Santiago Maldonado <santiagomaldonadorojas64@gmail.com>
2023-02-25 10:09:38 -0500

    Se realizaron cambios en el repositorio

---
+++ Python/Pila.py
@@ -0,0 +1,17 @@
+
+class Pila:
+    def contructor(self,tamaño):
+        lista = [self.tamaño]
+        espejo = [self.tamaño]
+    def pop():        
+        lista [lista.length] = None
+        return True
+    def peek():
+        for i in range(lista.length):
+            if(lista[i] != None):
+
+            
+        else:
+            return False
+    def push(self, object):
+        
