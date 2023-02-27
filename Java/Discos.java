425633dc4ebc55fe80e264f274dfbaa2eba45fdd
Santiago Maldonado <santiagomaldonadorojas64@gmail.com>
2023-02-26 21:58:52 -0500

    Se agregaron archivos.

--- Java/Discos.java
+++ Java/Discos.java
@@ -4,36 +4,58 @@ public class Discos
 {
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Ingreso de la masa total: ");
-        double vt = sc.nextDouble(); // almacenado de masa total
-        System.out.print("Ingreso de la masa perdida: ");
-        double v0 = sc.nextDouble();// almacenado de la masa perdida
-        int i = 0;
-        if (Math.pow(2, 16) > v0 && Math.pow(2, 64) > vt)
-        {
-            while (true)
+        Scanner input = new Scanner(System.in);
+        double vt, v0;
+        while (true) {
+            try {
+                System.out.print("\nIngreso de masa total para discos: ");
+                vt = Double.parseDouble(input.nextLine());
+                System.out.print("Masa perdida en la forja de discos: ");
+                v0 = Double.parseDouble(input.nextLine());
+                if (vt <= v0)
+                {
+                    System.out.println("Ingrese cantidades validas");
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            catch (NumberFormatException e)
             {
+                System.out.println("¡Ingrese algo valido!");
+            }
+        }
+        int i = 0;
+        int valorAnterior = 0;
+        double diametroAnterior = 0;
+        if (Math.pow(2, 16) > v0 && Math.pow(2, 64) > vt) {
+            while (true) {
                 i++;
-                double y = i * i; // calculo de casillas i = 2, la matriz sera de 4 casillas por que y = 2 * 2 = 4
-                double a = (vt / y) - v0; // calculo previo para verfificar que dentro de la raiz no sea negativo
+                int y = i * i;
+                double a = (vt / y) - v0;
                 if (a >= 0)
                 {
-                    System.out.println("Hace el calculo del diametro");
+                    System.out.println("Hace el calculo del diamextro");
                     double diametro = 0.3 * Math.sqrt(vt / y - v0);
                     System.out.println("Filas: " + i);
                     System.out.println("Diametro: " + diametro);
-                    double discos = diametro * y;
-                    if (discos > Math.pow(10, 3))
-                    {
-                        discos = discos % Math.pow(10, 3);
+                    int discos = (int) (diametro * y);
+                    System.out.println("Discos: " + discos);
+                    if (discos > (Math.pow(10, 3))) {
+                        discos %= Math.pow(10, 3);
                     }
-                    if (discos < Math.pow(10, 3)) 
-                    {
-                        System.out.println("Discos: " + discos);
+					if (discos < valorAnterior) {
+                        System.out.println("Fila: " + (i - 1));
+                        System.out.println("Maximo número estirado: " + valorAnterior);
+                        System.out.println("Diametro maximo: " + diametroAnterior);
+                        break;
                     }
+                    valorAnterior = discos;
+                    diametroAnterior = diametro;
                 }
-                else 
+                else
                 {
                     break;
                 }
@@ -41,3 +63,4 @@ public class Discos
         }
     }
 }
+
