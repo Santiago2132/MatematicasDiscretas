da0225c7d76c8c8adf4f0876c6c07f6597867eee
Santiago Maldonado <santiagomaldonadorojas64@gmail.com>
2023-03-01 09:37:18 -0500

    Se termino el algoritmo de cuadrados medios.

--- Python/AlgoritmoCuadradosMedios.py
+++ Python/AlgoritmoCuadradosMedios.py
@@ -1,21 +1,19 @@
-def cuadrados_medios(semilla, digitos):
-    # Calcula el número de dígitos necesarios para el cuadrado de la semilla
-    d = len(str(semilla**2))
-    # Genera números pseudoaleatorios a partir de la semilla
-    for i in range(digitos):
-        # Calcula el cuadrado de la semilla y lo convierte a una cadena de caracteres
-        cuadrado = str(semilla**2)
-        # Añade ceros a la izquierda si es necesario para obtener un número de dígitos igual a d
-        while len(cuadrado) < 2*d:
-            cuadrado = '0' + cuadrado
-        # Extrae el número de dígitos deseados del centro del cuadrado y lo convierte a un número entero
-        inicio = (len(cuadrado) - d) // 2
-        fin = inicio + d
-        semilla = int(cuadrado[inicio:fin])
-        # Normaliza el número generado entre 0 y 1
-        aleatorio = semilla / (10**d)
-        # Devuelve el número generado
-        yield aleatorio
-a = float(input("\nIngreso de semilla: "))
-b = float(input(" Cantidad de digitos: "))
-print(cuadrados_medios(a,b))
+while True:
+    semilla = input("Escriba semilla: ")
+    tam1 = len(semilla) #Determina el tamaño de la semilla
+    print("Cantidad de dígitos: ", tam1) #Dependiendo del tamaño de la semilla sera la longitud de los números generados
+    numero1 = int(semilla)
+    for i in range(10):
+        numero2 = numero1**2 #Se realiza la operación del cuadrado de la semilla
+        snumero2 = str(numero2) #Transforma el número generado en una string
+        tam2 = len(snumero2) #Calcula el tamaño del número generado
+        primerc = int((tam2 - tam1) / 2) #Determina a partir de cual número se extrae la cadena de tam1 del calculo generado
+        snumero3 = snumero2[primerc:primerc+tam1]#Se arma la cadena a partir del primer digito
+        print ("{}.  {}".format(i,snumero3)) #Imprime de manera seccionada entre el número de i y el número generado
+        numero1 = int(snumero3)
+    try:
+        option = int(input("Ingresa '1' si quieres generar más digitos seudo aleatorios:  "))
+        if option != 1:
+            break;
+    except ValueError:
+        print("¡Ingrese un número!")
