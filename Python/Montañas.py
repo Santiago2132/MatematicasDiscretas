0b48e2a3910af44d66bdd05be3ba7ab9455c6a47
Santiago Maldonado Rojas <santiagomaldonadorojas64@gmail.com>
2023-03-18 11:44:27 -0500

    Se agrego el ejercicio

---
+++ Python/Montañas.py
@@ -0,0 +1,21 @@
+print("      /\ \n     /_ \ \n    / \/ \ \n   /      \ \n  /        \ \n /          \ \n/            \Montañas")
+#Funciones:
+def triangulo(x1,y1,x2,y2):
+    tiempo = 0
+    if(y2>y1):
+        y3 = y1
+        x3 = x2
+    if(y1>y2):
+         x1
+
+    return tiempo
+#Atributos
+
+cantCord = int(input("Cantidad de coordenadas:  "))#Ingreso de coordenadas
+lista1 = []
+lista2 = []
+for i in range(cantCord):#Ingreso de puntos
+    x = int(input("Ingrese la coordenada en x: "))
+    y = int(input("Ingrese la coordenada en y: "))
+    lista1.append(x)
+    lista2.append(y)
