4b94af1b21ef0c3de14cb429db8671f4de9cb8e7
Santiago Maldonado Rojas <santiagomaldonadorojas64@gmail.com>
2023-03-20 12:38:52 -0500

    cambios

--- Python/Prueba.py
+++ Python/Prueba.py
@@ -1,3 +1,49 @@
-for i in range(9):
-    print("A")
-    #break
+import math
+
+print("      /\n     /_ \n    / \\/ \n   /      \\ \n  /        \\ \n /          \\\n/            \\Montañas")
+
+#Funciones:
+def tiempo (d,v): # Calcula el tiempo en el cual 
+    tiempo = d/v
+    return tiempo
+def formulaDistancia(x1,y1,x2,y2): #Función para la distancia entre puntos: 
+    x = (x2-x1)**2
+    y = (y2-y1)**2
+    distancia = math.sqrt(x + y)
+    return distancia        
+
+#Atributos si se requiere de entrada
+vw = int(input("Ingrese la velocidad de caminata: "))
+vc = int(input("Ingrese la velocidad cuando se excava el tunel: "))
+cantCord = int(input("Cantidad de coordenadas:  "))#Ingreso de coordenadas
+lista1 = []
+lista2 = []
+for i in range(cantCord):#Ingreso de puntos
+    x = int(input("Ingrese la coordenada en x: "))
+    y = int(input("Ingrese la coordenada en y: "))
+    lista1.append(x)
+    lista2.append(y)
+
+i=0
+tiempo = 0
+penultimo = len(lista1)-1
+while True:
+    if i != penultimo:   
+        if lista2[i+1]> lista2[i]:
+            if lista2[i] == lista2[i+2]:
+                d1 = formulaDistancia(lista1[i],lista2[i],lista1[i+1],lista2[i+1])
+                d2 = formulaDistancia(lista1[i],lista2[i],lista1[i+2],lista2[i+2])
+                t1 = tiempo(d1,vw)
+                t2 = tiempo(d2,vc)
+                if (t2 > t1):
+                    tiempo = tiempo + t1
+                    i+=1
+                else:
+                    tiempo = tiempo + t2
+                    i+=2
+    else:
+        d = formulaDistancia(lista1[i],lista2[i],lista1[i+1],lista2[i+1])
+        caltiemp = d / vw
+        tiempo = tiempo + caltiemp
+        break
+print(tiempo)
--- Python/Pruebas.py
+++ Python/Pruebas.py
@@ -1,54 +1,43 @@
-#Algoritmo de cuadrados medios flexible 
-def algoritmo_cuadrados_medios(semilla, cant_numeros): # primera ingresa la semilla y luego la cantidad de número seudo aleatorios a generar
-    resultado = []
-    tam1 = len(semilla) #Determina el tamaño de la semilla
-    numero1 = int(semilla) #Conversor de la semilla a un entero
-    for i in range(cant_numeros):
-        numero2 = numero1**2 #Se realiza la operación del cuadrado de la semilla
-        snumero2 = str(numero2) #Transforma el número generado en una string
-        tam2 = len(snumero2) #Calcula el tamaño del número generado
-        primerc = int((tam2 - tam1) / 2)#Determina a partir de cual número se extrae la cadena de tam1 del calculo generado
-        snumero3 = snumero2[primerc:primerc+tam1]#Se arma la cadena a partir del primer digito
-        resultado.append(int(snumero3))
-        numero1 = int(snumero3)
-    return resultado
-#Función para saber si tiene 3 números iguales
-def tredigitosiguales(num): #Ya funciona
-    digitos = []
-    # Convertir el número en una cadena y recorrer cada dígito
-    for digito in str(num):
-        # Convertir el dígito en un entero y agregarlo a la lista
-        digitos.append(int(digito))
-    # Creación de la función
-    for i in range(len(digitos)):
-        cont = 0
-        for j in range(len(digitos)):
-            if digitos[i] == digitos[j]: #Se recorre la misma lista con el fin de comparar si existen iguales
-                cont += 1#Con ello almacenando y si llega a 3, retornar verdadero
-            if cont >= 3:
-                return True
-    return False
-#Programa principal
-while True:#Falta culminar la salida de los números aleatorios.
-    try:
-        seed = int(input("Ingrese una semilla de 5 a 7 dígitos: "))
-        if len(str(seed)) < 5 or len(str(seed)) > 7:#Verifica la cantidad de digitos que tiene el número 
-            print("Error: la semilla debe tener entre 5 y 7 dígitos")
-            continue
-        elif len(set(str(seed))) == 1: #Verifica que no tenga todos los digitos iguales
-            print("Error: la semilla no puede tener todos los dígitos iguales")
-            continue
-        elif any(str(seed)[i:i+3] in '0123456789' for i in range(len(str(seed))-2)): # verifica la secuencia no sea igual
-            print("Error: la semilla no puede tener números consecutivos")
-            continue
-        elif tredigitosiguales(seed): # Que no tenga 3 digitos repetidos
-            print("Error: la semilla no puede tener tres dígitos iguales")
-            continue
-        else:
-            seed = str(seed) #cambia de int a string, aunque se podría cambiar pero buenoooooo
-            print(algoritmo_cuadrados_medios(seed, 36)) #El segundo numerito es para la cantidad de numeros a dar, solamente que no recuerdo el valor exacto entonces puse el mio          
-    except ValueError:
-        print("Error: el valor ingresado no es un número")
-        continue
-    else:
-        break
+#Programa algebra lineal
+import numpy as np
+
+ALPHABET = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+
+def decrypt(ciphertext, key):
+    n = len(key)
+    det = int(round(np.linalg.det(key)))
+    det_inv = pow(det, -1, len(ALPHABET))
+    key_inv = (det_inv * np.round(det * np.linalg.inv(key)).astype(int) % len(ALPHABET)).astype(int)
+    
+    plaintext = ""
+    
+    # Create matrix from ciphertext
+    ciphertext_matrix = []
+    row = []
+    for i in range(len(ciphertext)):
+        row.append(ALPHABET.index(ciphertext[i]))
+        if (i+1) % n == 0:
+            ciphertext_matrix.append(row)
+            row = []
+    
+    # Fill remaining spaces with 'X'
+    if len(row) > 0:
+        while len(row) < n:
+            row.append(ALPHABET.index('X'))
+        ciphertext_matrix.append(row)
+    
+    # Decrypt matrix
+    decrypted_matrix = np.dot(key_inv, np.transpose(ciphertext_matrix)) % len(ALPHABET)
+    
+    # Convert decrypted matrix to plaintext
+    for j in range(decrypted_matrix.shape[1]):
+        for i in range(n):
+            plaintext += ALPHABET[decrypted_matrix[i][j]]
+    
+    return plaintext
+
+key_input = input("Introduce la matriz clave (separada por comas y espacios): ")
+key_matrix = [[int(num) for num in row.split()] for row in key_input.split(",")]
+ciphertext_input = input("Introduce el mensaje cifrado: ")
+
+print(decrypt(ciphertext_input.upper(), key_matrix))
