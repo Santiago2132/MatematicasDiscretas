4106ee02c802dea70df71650efd2795a77142940
Santiago Maldonado Rojas <santiagomaldonadorojas64@gmail.com>
2023-03-22 18:23:46 -0500

    Se corrigio y probo el codigo de calculo de distancais entre montañas

--- Python/Montañas.py
+++ Python/Montañas.py
@@ -1,54 +1,46 @@
 import math
 print("      /\ \n     /_ \ \n    / \/ \ \n   /      \ \n  /        \ \n /          \ \n/            \Montañas")
 #Funciones:
-def tiempo (d,v): # Calcula el tiempo en el cual 
-    tiempo = d/v
-    return tiempo
-def formulaDistancia(x1,y1,x2,y2): #Función para la distancia entre puntos: 
-    x = (x2-x1)**2
-    y = (y2-y1)**2
-    distancia = math.sqrt(x - y)
-    return distancia        
+def calcularDistanciaEntrePuntos(x1, x2, y1, y2):
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 
-#Atributos si se requiere de entrada
-vw = int(input("Ingrese la velocidad de caminata: "))
-vc = int(input("Ingrese la velocidad cuando se excava el tunel: "))
-cantCord = int(input("Cantidad de coordenadas:  "))#Ingreso de coordenadas
-lista1 = []
-lista2 = []
-for i in range(cantCord):#Ingreso de puntos
-    x = int(input("Ingrese la coordenada en x: "))
-    y = int(input("Ingrese la coordenada en y: "))
-    lista1.append(x)
-    lista2.append(y)
+def calcularDistanciaHorizontal(x1, x2):
+    return abs(x2 - x1)
 
-i=0
-tiempo = 0
-penultimo = len(lista1)-1
-while True:
-    if i != penultimo:   
-        if lista2[i+1]> lista2[i]:
-            if lista2[i] == lista2[i+2]:
-                d1 = formulaDistancia(lista1[i],lista2[i],lista1[i+1],lista2[i+1])
-                d2 = formulaDistancia(lista1[i],lista2[i],lista1[i+2],lista2[i+2])
-                t1 = tiempo(d1,vw)
-                t2 = tiempo(d2,vc)
-                if (t2 > t1):
-                    tiempo = tiempo + t1
-                    i+=1
-                else:
-                    tiempo = tiempo + t2
-                    i+=2
+cantidad = int(input("Ingrese la cantidad de puntos: "))
+coordenadasX = []
+coordenadasY = []
+
+for i in range(cantidad):
+    x = float(input(f"Ingrese la coordenada X del punto {i+1}: "))
+    y = float(input(f"Ingrese la coordenada Y del punto {i+1}: "))
+    coordenadasX.append(x)
+    coordenadasY.append(y)
+
+vc = float(input("Ingrese la velocidad de cavar: "))
+vw = float(input("Ingrese la velocidad de caminar: "))
+
+distancia = 0
+tiempoMin = 0
+
+for i in range(cantidad):
+    if i+1 != cantidad:
+        distanciaMont = calcularDistanciaEntrePuntos(coordenadasX[i], coordenadasX[i+1], coordenadasY[i], coordenadasY[i+1])
+        if ((coordenadasY[i] > 0 and coordenadasY[i+1] < 0) or (coordenadasY[i] < 0 and coordenadasY[i+1] > 0) or (coordenadasY[i] == coordenadasY[i+1])):
+            distancia += distanciaMont
+            tiempoMin += distanciaMont / vw
         else:
-            d = formulaDistancia(lista1[i],lista2[i],lista1[i+1],lista2[i+1])
-            caltiemp = d / vw
-            tiempo = tiempo + caltiemp               
-            i+=1
-    else:
-        d = formulaDistancia(lista1[i],lista2[i],lista1[i+1],lista2[i+1])
-        caltiemp = d / vw
-        tiempo = tiempo + caltiemp
-        break
-    
-
-print(tiempo)
+            distanciaHorizontal = calcularDistanciaHorizontal(coordenadasX[i], coordenadasX[i+1])
+            if vw > vc or vw < vc:
+                tiempoMont = distanciaMont / vw
+                tiempoCueva = distanciaHorizontal / vc
+                if tiempoMont > tiempoCueva:
+                    tiempoMin += tiempoCueva
+                else:
+                    tiempoMin += tiempoMont
+            if vw == vc:
+                if distanciaMont > distanciaHorizontal:
+                    tiempoMin += distanciaHorizontal / vc
+                else:
+                    tiempoMin += distanciaMont / vw
+print("El tiempo es: ", tiempoMin)
