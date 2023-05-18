3ee8ba7a6b33f8893b8d94c6a1c7f36063a58999
Santiago Maldonado Rojas <santiagomaldonadorojas64@gmail.com>
2023-05-18 14:12:07 -0500

    Se modifico Kruskal para pruebas

--- Python/Algoritmos/Kruskal.py
+++ Python/Algoritmos/Kruskal.py
@@ -93,24 +93,16 @@ def visualizar_arbol(grafo, arbol):
     plt.axis('off') # Ocultamos los ejes
     plt.show() # Mostramos el grafo
     
-grafo = [[0, 1, 3, 0],
-         [1, 0, 2, 4],
-         [3, 2, 0, 5],
-         [0, 4, 5, 0]]
-visualizar_grafo(grafo)
-arbol_expansion = kruskal(grafo)
-visualizar_arbol(grafo,arbol_expansion)
-imprimir_grafo_y_arbol(grafo, arbol_expansion)
 
-grafo2 = [[0,4,0,0,0,0,0,9,0],
-          [4,0,9,0,0,0,0,11,0],
-          [0,9,0,7,0,4,0,0,2],
-          [0,0,7,0,10,15,0,0,0],
-          [0,0,0,10,0,11,0,0,0],
-          [0,0,4,15,11,0,2,0,0],
-          [0,0,0,0,0,2,0,1,6],
-          [9,11,0,0,0,0,1,0,7],
-          [0,0,2,0,0,0,6,7,0]]
+#grafo2 = [[0,0,2,2,4,0,3],[0,0,6,7,0,4,3],[2,6,0,0,0,5,2],[2,7,0,0,5,0,6],[4,0,0,5,0,0,0],[0,4,5,0,0,0,0],[3,3,2,6,0,0,0,0]]
+#grafo2 = [[0,0,8,2,4,0,3],[0,0,5,2,0,4,3],[8,5,0,0,0,5,2],[2,2,0,0,5,0,5],[4,0,0,5,0,0,0],[0,4,5,0,0,0,0],[3,3,2,5,0,0,0]]
+grafo2 = [[0,0,7,2,4,0,3],
+          [0,0,2,4,0,4,3],
+          [7,2,0,0,0,5,2],
+          [2,4,0,0,5,0,2],
+          [4,0,0,5,0,0,0],
+          [0,4,5,0,0,0,0],
+          [3,3,2,2,0,0,0]]
 visualizar_grafo(grafo2)
 arbol_expansion2 = kruskal(grafo2)
 visualizar_arbol(grafo2,arbol_expansion2)
